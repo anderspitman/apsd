0154dbb34aa92fc0de420821af791840e8a45509
Anders Pitman <&ers@apitman.com>
2024-05-13 15:50:02 -0600

    Add ripgrep

--- build.bash
+++ build.bash
@@ -23,6 +23,12 @@ tar -xf zoxide.tar.gz -C zoxide
 mv zoxide/zoxide apsd/bin/
 rm -rf zoxide zoxide.tar.gz
 
+RIPGREP_VERSION=14.1.0
+http_get https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl.tar.gz ripgrep.tar.gz
+tar -xf ripgrep.tar.gz
+mv ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl/rg apsd/bin/
+rm -rf ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl/ ripgrep.tar.gz
+
 #NEOVIM_VERSION=0.9.5
 #curl --fail -Lo nvim https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim.appimage
 #chmod +x nvim
