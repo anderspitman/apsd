8bbad5236ccf0ffaad59613c089334b7c825e269
Anders Pitman <&ers@apitman.com>
2024-05-13 15:37:05 -0600

    Add init.vim

--- build.bash
+++ build.bash
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-mkdir -p apsd/bin
+set -eu
+
+function http_get {
+    uri=$1
+    dest=$2
+    curl --fail -Lo $dest $uri
+}
+
+mkdir -p apsd/bin/
 
 FZF_VERSION=0.52.0
-curl -o fzf.tar.gz -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz
+http_get https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz fzf.tar.gz
 tar -xf fzf.tar.gz
-mv fzf apsd/bin
+mv fzf apsd/bin/
 rm -rf fzf.tar.gz
-
 
 ZOXIDE_VERSION=0.9.4
 mkdir -p zoxide
-curl -o zoxide.tar.gz -L https://github.com/ajeetdsouza/zoxide/releases/download/v${ZOXIDE_VERSION}/zoxide-${ZOXIDE_VERSION}-x86_64-unknown-linux-musl.tar.gz
+http_get https://github.com/ajeetdsouza/zoxide/releases/download/v${ZOXIDE_VERSION}/zoxide-${ZOXIDE_VERSION}-x86_64-unknown-linux-musl.tar.gz zoxide.tar.gz
 tar -xf zoxide.tar.gz -C zoxide
-mv zoxide/zoxide apsd/bin
+mv zoxide/zoxide apsd/bin/
 rm -rf zoxide zoxide.tar.gz
 
-cp install.bash apsd
-cp install.posix apsd
+#NEOVIM_VERSION=0.9.5
+#curl --fail -Lo nvim https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim.appimage
+#chmod +x nvim
+#mv nvim apsd/bin/
+
+cp install.bash apsd/
+cp install.posix apsd/
+cp init.vim apsd/
 
 APSD_VERSION=$(git describe --tags)
 
-tar -czvf apsd_${APSD_VERSION}.tar.gz apsd/bin apsd/install.bash apsd/install.posix
+tar -czvf apsd_${APSD_VERSION}.tar.gz apsd/bin apsd/install.bash apsd/install.posix apsd/init.vim
 
 rm -rf apsd
